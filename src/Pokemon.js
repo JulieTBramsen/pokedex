d5e378da533424de8131cd323f8456dc8802ac0e
juli175h <jbramsen1212@gmail.com>
2024-04-21 15:20:28 +0200

    more routing

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -8,7 +8,7 @@ export default function Pokemon(pm){
 
     return (
         <>
-        <Link className='link' to={"/pokedex/"+pm.name}>
+        <Link className='link' to={"/"+pm.name}>
             <div className={classnames('pokecard', pm.types[0].type.name)}>
                <h1>{pm.id+": "+pm.name}</h1>
                <img src={pm.sprites.front_default} alt={pm.name}/>
